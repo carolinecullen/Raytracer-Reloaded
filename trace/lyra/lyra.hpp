8545c592f2071da63d552fe160cf0afdf62c5e37
carolinecullen <cullen.caroline@gmail.com>
2021-07-21 23:20:16 -0700

    Add command line parsing using Lyra.

---
+++ trace/lyra/lyra.hpp
@@ -0,0 +1,28 @@
+// Copyright 2018-2020 René Ferdinand Rivera Morell
+// Copyright 2017 Two Blue Cubes Ltd. All rights reserved.
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef LYRA_LYRA_HPP
+#define LYRA_LYRA_HPP
+
+#include "version.hpp"
+
+#include "arg.hpp"
+#include "arguments.hpp"
+#include "cli_parser.hpp"
+#include "cli.hpp"
+#include "command.hpp"
+#include "exe_name.hpp"
+#include "group.hpp"
+#include "help.hpp"
+#include "literal.hpp"
+#include "main.hpp"
+#include "opt.hpp"
+#include "option_style.hpp"
+#include "parser_result.hpp"
+#include "parser.hpp"
+#include "val.hpp"
+
+#endif // LYRA_HPP_INCLUDED
--- trace/tracethoserays.cpp
+++ trace/tracethoserays.cpp
@@ -1,3 +1,42 @@
-int main {
+// My name is Caroline Cullen and this is Raytracer: Reloaded.
+#include <cstdlib>
+#include <iostream>
+// I love the name of this CLI parser, Lyra is a BA in HDM.
+#include "lyra/lyra.hpp"
+
+int main(int argc, const char **argv) {
+  // Values from the command line parser.
+  // Big shout out to the people who make CLI parsers.
+  int heightInPixels = 430;
+  int widthInPixels = 575;
+  std::string nameForImage = "reloaded.png";
+  std::string test;
+
+  // Required for Lyra to provide help.
+  bool show_help = false;
+
+  auto cli = lyra::cli_parser() | lyra::help(show_help) |
+             lyra::opt(heightInPixels, "height")["-h"]["--height"](
+                 "Height in pixels! (Default to 430 pixels)") |
+             lyra::opt(widthInPixels, "width")["-w"]["--width"](
+                 "Width in pixels! (Default to 575 pixels)") |
+             lyra::opt(nameForImage, "name")["-n"]["--name"](
+                 "Name the beautiful image that will bring light and color to "
+                 "your life. (Default to reloaded.png)") |
+             lyra::opt(test, "test")["-t"]["--test"]("Test this thang!");
+
+  auto result = cli.parse({argc, argv});
+  if (!result) {
+    std::cerr << "Error in command line: " << result.errorMessage()
+              << std::endl;
+    return 1;
+  }
+
+  // Someone asked for help and now we gon give it to em.
+  if (show_help) {
+    std::cout << cli << "\n";
     return 0;
+  }
+
+  return 0;
 }
